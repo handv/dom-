816c6c861facf24a43daeef15ee9a7aa3470711d
handw <handv@163.com>
2017-05-29 19:02:59 +0800

    第12章contact页面编写

--- 第12章/scripts/global.js
+++ 第12章/scripts/global.js
@@ -5,6 +5,8 @@ addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
 addLoadEvent(stripeTable);
 addLoadEvent(highlightRows);
+addLoadEvent(focusLabels);
+addLoadEvent(prepareForms);
 
 function addLoadEvent(func) {
     var oldLoad = window.onload;
@@ -195,7 +197,7 @@ function stripeTable() {
     if (!document.getElementsByTagName('tbody')) return false;
     if (!document.getElementsByTagName('tr')) return false;
     var table = document.getElementsByTagName('tbody')[0];
-    var trs = table.getElementsByTagName('tr');
+    var trs = document.getElementsByTagName('tr');
     for (var i = 0; i < trs.length; i++) {
         if (i % 2 != 0) {
             addClass(trs[i], 'odd');
@@ -203,20 +205,150 @@ function stripeTable() {
     }
 }
 // live页面，鼠标悬浮时显示高亮
-function highlightRows(){
-	if (!document.getElementsByTagName) return false;
+function highlightRows() {
+    if (!document.getElementsByTagName) return false;
     if (!document.getElementsByTagName('tbody')) return false;
     if (!document.getElementsByTagName('tr')) return false;
     var table = document.getElementsByTagName('tbody')[0];
-    var trs = table.getElementsByTagName('tr');
+    var trs = document.getElementsByTagName('tr');
     for (var i = 0; i < trs.length; i++) {
-    	trs[i].oldClassName = trs[i].className;
-        trs[i].onmouseover = function(){
-        	addClass(this,'highlight');
+        trs[i].oldClassName = trs[i].className;
+        trs[i].onmouseover = function() {
+            addClass(this, 'highlight');
         }
-        trs[i].onmouseout = function(){
-        	// 还是这个方法简单有效，比用什么删除class的方法强
-        	this.className = this.oldClassName;
+        trs[i].onmouseout = function() {
+            // 还是这个方法简单有效，比用什么删除class的方法强
+            this.className = this.oldClassName;
         }
     }
+}
+// contact页面，点击label焦点在input上
+function focusLabels() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementsByTagName('fieldset')) return false;
+    if (!document.getElementsByTagName('label')) return false;
+    var fieldset = document.getElementsByTagName('fieldset')[0];
+    var labels = fieldset.getElementsByTagName('label');
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].onclick = function() {
+            var id = this.getAttribute("for");
+            var input = document.getElementById(id);
+            if (!input) return false;
+            input.focus();
+        }
+    }
+}
+// contact页面，placeholder的实现
+function resetFields(whichform) {
+    if (Modernizr.input.placeholder) return;
+    for (var i = 0; i < whichform.elements.length; i++) {
+        var element = whichform.elements[i];
+        element.txt = element.getAttribute('placeholder');
+        element.onfocus = function() {
+            //只有value和placeholder值一致，才为空
+            if (this.placeholder == this.txt) {
+                this.placeholder = "";
+            }
+        }
+        element.onblur = function() {
+            if (this.placeholder == '') {
+                this.placeholder = this.txt;
+            }
+        }
+        element.onblur(); //为什么要立即调用
+    }
+}
+// forms 集合返回当前页面所有表单的数组集合
+function prepareForms() {
+    for (var i = 0; i < document.forms.length; i++) {
+        var thisform = document.forms[i];
+        resetFields(thisform);
+        //form的submit方法
+        thisform.onsubmit = function() {
+            if (!validateForm(this)) return false;
+            var article = document.getElementsByTagName('article')[0];
+            //如果方法管用，就更改article的内容，返回false不用跳转
+            if (submitFormWithAjax(this, article)) return false;
+            //否则,返回true，跳转到submit.html页面
+            return true;
+        }
+    }
+}
+// contact页面，验证表单内容是否为空
+function isFilled(field) {
+    if (field.value.replace(' ', '').length == 0) return false;
+    var placeholder = field.placeholder;
+    return (field.value != placeholder);
+}
+// contact页面，验证email
+function isEmail(field) {
+    return (field.value.indexOf('@') != -1 && field.value.indexOf('.') != -1);
+}
+// contact页面，验证form表单
+function validateForm(whichform) {
+    for (var i = 0; i < whichform.elements.length; i++) {
+        var element = whichform.elements[i];
+        if (element.required) {
+            if (!isFilled(element)) {
+                alert('Please fill in the ' + element.name + ' field.');
+                return false;
+            }
+            if (element.type == 'email') {
+                if (!isEmail(element)) {
+                    alert('The ' + element.name + ' field must be a valid email address');
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+// loading图片
+function displayAjaxLoading(element) {
+    //删除掉element的所有子元素
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
+    var content = document.createElement('img');
+    content.src = 'images/loading.gif';
+    content.alt = 'Loading...';
+    element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform, thetarget) {
+    var request = new XMLHttpRequest();
+    displayAjaxLoading(thetarget);
+    var dataParts = [];
+    for (var i = 0; i < whichform.elements.length; i++) {
+        var thiselement = whichform.elements[i];
+        if (thiselement.name) {
+            dataParts.push(thiselement.name + '=' + encodeURIComponent(thiselement.value));
+        }
+    }
+    var data = dataParts.join('&');
+
+    request.open('POST', whichform.action, true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.onreadystatechange = function() {
+        //readystatus,HTTP 请求的状态
+        if (request.readyState == 4) {
+            //status,由服务器返回的 HTTP 状态代码
+            if (request.status == 200) {
+                //responseText,目前为止为服务器接收到的响应体（不包括头部），或者如果还没有接收到数据的话，就是空字符串。
+                var matches = request.reponseText.match(/<article>(.*)<\/article>/);
+                if (matches.length > 0) {
+                    thetarget.innerHTML = matches[1]; //[0]
+                } else {
+                    thetarget.innerHTML = '<p>Oops, there was an error. Sorry!</p>';
+                }
+            } else {
+                //statusText,这个属性用名称而不是数字指定了请求的 HTTP 的状态代码
+                thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+            }
+        }
+    };
+    //send(),发送 HTTP 请求，使用传递给 open() 方法的参数，以及传递给该方法的可选请求体。
+    //发送post方法的请求体
+    request.send(data);
+    return true;
 }
